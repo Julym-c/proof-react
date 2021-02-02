89e400f394f0ecc94d213fe5e9cbf2bfa5a69364
July Muñoz <julita_056@hotmail.com>
2021-02-02 15:05:50 -0500

    ADD: readme

--- src/components/searcher/searcher.js
+++ src/components/searcher/searcher.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './search.css'
 
 class Searcher extends React.Component {
+    // busqueda captura los datos que el usuario escribe
+    state={
+        busqueda: ''
+    }
+
+    onChange= async e=> {
+        e.persist();
+        await this.setState({busqueda: e.target.value});
+        console.log(this.state.busqueda)
+        }
+
     render(){
         return (
             <div className="container__search">
-                <input placeholder="Search other cities..."></input>
+                <input 
+                    placeholder="Search other cities..." 
+                    value={this.state.busqueda}
+                    onChange={this.onChange}> 
+                </input>
             </div>
         )
     }
