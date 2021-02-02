f28fac364c02c671e9f2786adb8c2687ccb9b323
July Muñoz <julita_056@hotmail.com>
2021-02-02 13:46:06 -0500

    ADD: fetch api display

--- src/components/information/display.js
+++ src/components/information/display.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import './info.css'
 
+
+
 class Information extends React.Component {
+    // fecth api
+    state = {
+        current_city: '',
+        icon:'',
+        description:'',
+        main:{}
+    }
+
+    componentDidMount() {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=Medellin&lang=es&units=metric&APPID=838d87a9b63e966e5b3753b305b76dfa')
+        .then((res) => res.json())
+        .then( data => {
+            this.setState({current_city: data})
+            this.setState({icon: this.state.current_city.weather[0].icon})
+            this.setState({main: this.state.current_city.main})
+            this.setState({description: this.state.current_city.weather[0].description})
+            //console.log(data)
+            //console.log(this.state.current_city.weather[0].icon)
+            console.log(this.state.current_city.weather[0].description)
+        })
+    }
+    // renderizar
     render(){
         return ( 
             <section className="display">
                 <p className="display__text">Current city</p>
-                <p className="display__title">Medellin.CO</p>
+                <p className="display__title">{this.state.current_city.name}</p>
                 <div className="display__information">
-                    <span className="information__icon"></span>
+                    <img src={'http://openweathermap.org/img/wn/'+this.state.icon+'@2x.png'}alt='icon' className="information__icon"/>
                     <div className="information__weather">
-                        <p className="weather__text"> 28 ºC</p>
-                        <p className="weather__subtext"> sunny</p>
+                        <p className="weather__text">{this.state.main.temp} ºC</p>
+                        <p className="weather__subtext"> {this.state.description}</p>
                     </div>
                 </div>
                 <div className="display__text-all">
-                    <p className="text">Cali,co</p>
-                    <p className="text">Sunny</p>
-                    <p className="text">Cali,co</p>
-                    <p className="text">Sunny</p>
+                    <p className="text"> {this.state.main.temp_min} ºC <span>Temp.minimun</span></p>
+                    <p className="text">{this.state.main.temp_max} ºC <span>Temp.maximun</span></p>
+                    <p className="text">{this.state.main.feels_like} ºC <span>Feels like</span></p>
+                    <p className="text">{this.state.main.humidity} ºC <span>Humidity</span></p>
                 </div>
             </section>
             )
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Cards from './components/cards/cards'
 import Information from './components/information/display'
 import Searcher from './components/searcher/searcher'
 
+
 render(<Cards/>, document.querySelector('#view__cards'));
 render(<Information/>, document.querySelector('#details'));
 render(<Searcher/>, document.querySelector('#city__search'));
